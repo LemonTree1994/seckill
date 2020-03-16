254ee88b00afa3c2aca6700f59472a5a36e73907
LiHao <819045899@qq.com>
2020-03-17 02:53:21 +0800

    Update client.py

--- client.py
+++ client.py
@@ -6,21 +6,22 @@ import sys
 import getopt
 
 
-def buy(userid,goodid):
-    rpost = requests.get("http://127.0.0.1:9999/buy3", params={"userid":userid,"goodid":goodid})
+def buy(url,userid,goodid):
+    rpost = requests.get("http://127.0.0.1:9999/"+url, params={"userid":userid,"goodid":goodid})
     return rpost.text
 
 def dec_buy(pool_input):
-    return buy(pool_input[0],pool_input[1])
+    return buy(pool_input[0], pool_input[1], pool_input[2])
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 2:
-        print("<usage>:python client.py <user_num>")
+    if len(args) != 3:
+        print("<usage>:python client.py <url> <user_num>")
     else:
-        user_num = int(args[1])
+        url = args[1]
+        user_num = int(args[2])
         pool = ThreadPoolExecutor(max_workers=10,thread_name_prefix="req_")
         t1 = time.time()
-        res = pool.map(dec_buy,[(i,1)for i in range(user_num)])
+        res = pool.map(dec_buy,[(url,i,1)for i in range(user_num)])
         for r in res:
             print(r)
         print("耗时：", time.time()-t1)
